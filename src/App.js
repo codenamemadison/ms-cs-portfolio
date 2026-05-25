1d446de4e23ff052c7ad35e5e9210d97f4ce55b6
Madison Shimbo <42008799+codenamemadison@users.noreply.github.com>
2026-05-25 16:36:36 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/contact-confirmation" element={<ContactConfirmation/>}/>
+        // TODO: Create UI section for Iot Nation & HYBRD work
       </Routes>
     </Router>
     </>
